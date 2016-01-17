ee0702805ea19a7a247bc65dba34128c0e546cf5
Leon <lowpolygonum@gmail.com>
2016-01-17 13:41:15 +0100

    see sound max distance

---
+++ Assets/Scripts/Utils/SeeSound.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SeeSound : MonoBehaviour 
+{
+	AudioSource[] audioSourceArray;
+
+	void Awake ()
+	{
+		audioSourceArray = GameObject.FindObjectsOfType<AudioSource>();
+	}
+
+  void OnDrawGizmos() 
+  {
+  	if (audioSourceArray != null) {
+			Gizmos.color = Color.yellow;
+			for (int i = 0; i < audioSourceArray.Length; ++i) {
+				AudioSource audioSource = audioSourceArray[i];
+				Gizmos.DrawWireSphere(audioSource.transform.position, audioSource.maxDistance);
+			}
+		}
+	}
+}
