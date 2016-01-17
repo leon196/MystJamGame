c0692bbff4079e19a0f763a19a049f88de219d8d
Leon <lowpolygonum@gmail.com>
2016-01-17 22:26:17 +0100

    end

--- Assets/Scripts/Game/Transition.cs
+++ Assets/Scripts/Game/Transition.cs
@@ -120,7 +120,7 @@ public class Transition : MonoBehaviour {
 						break;
 					}
 					case Gate.TransitionType.Sphere : {
-						delay = 14f;
+						delay = 10f;
 						Shader.SetGlobalFloat("_IsSphereTransition", 1f);
 						break;
 					}
@@ -198,7 +198,7 @@ public class Transition : MonoBehaviour {
 			yield return 0;
 		}
 
-		yield return new WaitForSeconds(1f);
+		yield return new WaitForSeconds(5f);
 
 		timeElapsed = 0f;
 
